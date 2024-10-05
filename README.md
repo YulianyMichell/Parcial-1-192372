9c69e57e42f388a6e796fd8e6268aa74c9d428ba
Yuliany <ymsanchezb@ufpso.edu.co>
2024-10-05 16:36:09 -0500

    (doc)Readme:agregue descripcion del ejercicio de suma de numeros

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Parcial-1-192372
+Ejercicio de suma de un numero
+En este ejercicio hice fue pédirle al usuario un numero, luego cree un ciclo for para hacer la sumatoria.
---
+++ sumanumeros.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class sumanumeros {
+    public static void main(String[] args) {
+
+    Scanner scanner=new Scanner(System.in);
+    
+    System.out.println("Ingrese un numero de 3 cifras");
+    int numero=scanner.nextInt();
+    int [] numeros=3;
+  
+    for(int i=1;i<=numero;i++){
+    double suma=+numero;
+    }
+        
+    System.out.println("La suma es:"+suma);
+       scanner.close();
+    }
+}
